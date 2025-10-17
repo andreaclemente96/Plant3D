a26b571cc46a76866d7cdbfb828b20bb07d8dabd
andreaclemente96 <101564715+andreaclemente96@users.noreply.github.com>
2025-10-17 13:25:30 +0200

    Remove unnecessary comments and clean up code

--- HeadLockedUI.cs
+++ HeadLockedUI.cs
@@ -5,7 +5,7 @@ public class HeadLockedUI : MonoBehaviour
     public Transform targetTransform;
     public float distanceInFront = 1.5f;
     public float horizontalOffset = 0f;
-    public float verticalOffset = 0f; // Ahora controla la altura relativa a tu visión
+    public float verticalOffset = 0f; 
 
     void Start()
     {
@@ -17,16 +17,15 @@ public class HeadLockedUI : MonoBehaviour
 
     void Update()
     {
-        // Posición base frente a la cámara
         Vector3 targetPosition = targetTransform.position + targetTransform.forward * distanceInFront;
 
-        // Mueve el objeto de forma horizontal y vertical respecto a la visión de la cámara
+    
         Vector3 rightVector = targetTransform.right * horizontalOffset;
         Vector3 upVector = targetTransform.up * verticalOffset;
 
         transform.position = targetPosition + rightVector + upVector;
 
-        // Asegura que el objeto siempre mire a la cámara
         transform.rotation = Quaternion.LookRotation(transform.position - targetTransform.position);
     }
+
 }
